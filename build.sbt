7e4c4d4e5b6721521317d69101d69ef08ac65582
jiang <jiang7462582@126.com>
2016-02-15 14:26:55 +0800

    2.15

--- build.sbt
+++ build.sbt
@@ -2,18 +2,24 @@ name := """helloRxScala"""
 
 version := "1.0"
 
-scalaVersion := "2.10.4"
+scalaVersion := "2.11.6"
+
+
+
 
 
 libraryDependencies += "com.netflix.rxjava" % "rxjava-scala" % "0.19.1"
 
 libraryDependencies += "log4j" % "log4j" % "1.2.17"
 
-// Change this to another test framework if you prefer
-libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
+//// Change this to another test framework if you prefer
+//libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
+
+libraryDependencies += "org.apache.commons" % "commons-io" % "1.3.2"
 
 
 
 // Uncomment to use Akka
 //libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.3.11"
 
+ivyScala := ivyScala.value map { _.copy(overrideScalaVersion = true) }
--- src/main/scala/com/example/ObservablesItems.scala
+++ src/main/scala/com/example/ObservablesItems.scala
@@ -6,20 +6,74 @@ package com.example
 
 
 
-
 import rx.lang.scala._
+import scala.concurrent.Future
 import scala.concurrent.duration._
-object ObservablesItems extends App{
-//    val o = Observable.items("Pascal","Java","Scala")
-//    o.subscribe(p=>println("learn :" + p))
-//    o.subscribe(p=>println("forget :" + p))
+import scala.concurrent.ExecutionContext.Implicits.global
 
-    val o = Observable.timer(5.second)
-    o.subscribe(_=>println("Timeout!!"))
-    o.subscribe(_=>println("Another Timeout"))
-    println("begin")
-    Thread.sleep(6000)
-    println("end")
+//import org.apache.commons.io.monitor._
+
+
+
+
+trait Logging{
+    def log(s:String)
+    def warn(s:String) =log("Warn: "+s)
+    def error(s:String) = log("Error:" +s)
+}
+
+object ObservablesItems extends App with Logging {
+    override def log(s: String) = println(s)
+
+    //    val o = Observable.items("Pascal","Java","Scala")
+    //    o.subscribe(p=>println("learn :" + p))
+    //    o.subscribe(p=>println("forget :" + p))
+
+    //    val o = Observable.timer(1.second)
+    //    o.subscribe(_=>println("Timeout!!"))
+    //    o.subscribe(_=>println("Another Timeout"))
+    //    Thread.sleep(3000)
+
+    //    val exc = new RuntimeException
+    //
+    //    val o = Observable.items(1,2) ++ Observable.error(exc)
+    //    o.subscribe(
+    //        x => println("number:"+x),
+    //        t => println("error:" + t)
+    //    )
+    //    val classiscs = List("Good , bad , ugly","Titanic","Die Hard")
+    //    val movies = Observable.from(classiscs)
+    //    val vms = Observable.create[String]{ obs =>
+    //    obs.onNext("JVM")
+    //    obs.onNext("DartJVM")
+    //    obs.onNext("V8")
+    //    obs.onCompleted()
+    //    Subscription()
+    //}
+    //  vms.subscribe(log _, e =>log(s"oops - $e"),()=>log("Done!"))
+
+    //    val f = Future("Back to the Future(s)")
+    //    val o = Observable.create[String]{ obs =>
+    //        f.foreach{case s =>obs.onNext(s);obs.onCompleted()}
+    //        f.failed.foreach{case t =>obs.onError(t)}
+    //        Subscription()
+    //
+    //    }
+    //    o.subscribe(println(_))
+
+
+    //  Observable.from(Future("Hello World")).subscribe(println(_))
+
+
+//    val odds = Observable.interval(0.5.seconds).filter(_ % 2 == 1).map(n => s"number $n").take(5)
+//    odds.subscribe(println(_),e=>println(e.getCause),()=>println("no more data"))
+//
+//    Thread.sleep(5000)
+
+    val event = for(n<-Observable.from(0 to 10);if n %2 ==0) yield s"eventNum $n"
+    event.subscribe(println(_))
 
 
 }
+
+
